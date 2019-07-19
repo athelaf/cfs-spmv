ccec0eec0c5e0675a132fe29236542f5281facb1
Athena <athena@cslab.ece.ntua.gr>
2019-07-19 18:14:51 +0300

    [fix]: missing stuff

--- include/utils/platforms.hpp
+++ include/utils/platforms.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #ifdef _INTEL_COMPILER
 #define PRAGMA_IVDEP _Pragma("ivdep")
 #else
@@ -15,5 +17,10 @@ enum class Tuning { None, Aggressive };
 enum class Format { coo, csr, sss, hyb };
 
 inline int iceildiv(const int a, const int b) { return (a + b - 1) / b; }
+inline bool isEqual(double x, double y) {
+  const double epsilon = 1e-7 /* some small number such as 1e-5 */;
+  return abs(x - y) <= epsilon * abs(x);
+  // see Knuth section 4.2.2 pages 217-218
+}
 
 } // end of namespace util
--- src/csr_matrix.cpp
+++ src/csr_matrix.cpp
@@ -3,6 +3,10 @@
 namespace matrix {
 namespace sparse {
 
+#ifndef _USE_BARRIER
+std::atomic<bool> done[MAX_THREADS][MAX_COLORS];
+#endif
+
 // Explicit instantiations
 template class CSRMatrix<int, float>;
 template class CSRMatrix<int, double>;
